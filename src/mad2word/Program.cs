9cf12d71620d8776713e9da0756a3ef96c8e5809
Larry Golding <lgolding@microsoft.com>
2016-07-26 15:49:18 -0700

    Display banner

--- src/mad2word/Program.cs
+++ src/mad2word/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Licensed under the MIT License.
+// See the LICENSE file in the project root for license information.
 
-namespace mad2word
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+
+namespace Mad2Word
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Banner();
+        }
+
+        private static void Banner()
+        {
+            Assembly entryAssembly = Assembly.GetEntryAssembly();
+            IEnumerable<Attribute> attributes = entryAssembly.GetCustomAttributes();
+
+            var titleAttribute = attributes.Single(a => a is AssemblyTitleAttribute) as AssemblyTitleAttribute;
+            string programName = titleAttribute.Title;
+
+            string version = entryAssembly.GetName().Version.ToString();
+
+            Console.WriteLine(string.Format(CultureInfo.CurrentCulture, Resources.Banner, programName, version));
+            Console.WriteLine();
         }
     }
 }
